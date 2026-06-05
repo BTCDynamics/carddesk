29932e175b04608c75d6df7cf81dd1f24cfcebfc
BTCDynamics <trthompson@protonmail.com>
2026-06-05 07:08:23 -0500

    Add files via upload

--- helpers/card_code_helpers.py
+++ helpers/card_code_helpers.py
@@ -5,10 +5,10 @@ def generate_card_code():
     last_card = Card.query.order_by(Card.id.desc()).first()
 
     if not last_card:
-        return "CW-000001"
+        return "CD-000001"
 
     next_number = last_card.id + 1
 
-    return f"CW-{next_number:06d}"
+    return f"CD-{next_number:06d}"
 
 
